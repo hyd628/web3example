4cdfca28a9994e4f58bb12fcfda89e74b103e3e2
Henry (Wei) <hyd628@gmail.com>
2022-02-15 09:47:38 +0800

    Update transaction.js

--- transaction.js
+++ transaction.js
@@ -5,14 +5,14 @@ const Web3 = require('web3');
 */
 // Provider
 const providerRPC = {
-   moonriver: 'https://rpc.moonriver.moonbeam.network',
-   moonbase: 'https://rpc.testnet.moonbeam.network',
+   moonriver: 'https://rpc.api.moonriver.moonbeam.network',
+   moonbase: 'https://rpc.api.moonbase.moonbeam.network',
 };
 const web3 = new Web3(providerRPC.moonriver); //Change to correct network
 
 const account_from = {
    privateKey: '',
-   address: '0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac',
+   address: '0xbCE85847d4EE95DA1cF34c9e7415E253b0Dc36c3',
 };
 const addressTo = '0x44236223aB4291b93EEd10E4B511B37a398DEE55'; // Change addressTo
 
@@ -27,9 +27,10 @@ const deploy = async () => {
    // Sign Tx with PK
    const createTransaction = await web3.eth.accounts.signTransaction(
       {
-         gas: 50000,
+         maxPriorityFeePerGas: 50000,
          to: addressTo,
          value: web3.utils.toWei('0.00005', 'ether'),
+         type: "0x02"
       },
       account_from.privateKey
    );
